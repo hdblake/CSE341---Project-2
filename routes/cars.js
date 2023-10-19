d332c08ac32c9186eebf7304bea2866a8355a8f3
Hunter <hdblake333@gmail.com>
2023-10-19 01:03:04 -0500

    updated routes with validation

--- routes/cars.js
+++ routes/cars.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const carsController = require("../controller/cars");
+const { carValidation, validate } = require("../validation/validator");
 
 router.get("/", carsController.getCars);
 router.get("/:id", carsController.getOneCar);
-router.post("/", carsController.addCar);
-router.put("/:id", carsController.updateCar);
+router.post("/", carValidation(), validate, carsController.addCar);
+router.put("/:id", carValidation(), validate, carsController.updateCar);
 router.delete("/:id", carsController.deleteCar);
 
 module.exports = router;
