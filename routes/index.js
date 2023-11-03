719f8ebf7de1e5878a43b4c43e8cdae10fa1de26
= <=>
2023-11-03 15:01:45 -0500

    set up auth and config

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SESSION_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTHO_CLIENT_ID,
+  issuerBaseURL: process.env.AUTHO_ISSUER_BASE_URL
+};
+
+router.use(auth(config));
+router.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 
 router.use("/", require("./swagger"));
 router.use("/cars", require("./cars"));
