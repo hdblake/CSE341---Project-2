ec5098f8b6fb5cbcc92740fce5e8e8514261450a
= <=>
2023-11-04 15:26:37 -0500

    added requiresAuth

--- routes/cars.js
+++ routes/cars.js
@@ -1,11 +1,24 @@
 const router = require("express").Router();
 const carsController = require("../controller/cars");
 const { carValidation, validate } = require("../validation/validator");
+const { requiresAuth } = require("express-openid-connect");
 
-router.get("/", carsController.getCars);
-router.get("/:id", carsController.getOneCar);
-router.post("/", carValidation(), validate, carsController.addCar);
-router.put("/:id", carValidation(), validate, carsController.updateCar);
-router.delete("/:id", carsController.deleteCar);
+router.get("/", requiresAuth(), carsController.getCars);
+router.get("/:id", requiresAuth(), carsController.getOneCar);
+router.post(
+  "/",
+  requiresAuth(),
+  carValidation(),
+  validate,
+  carsController.addCar
+);
+router.put(
+  "/:id",
+  requiresAuth(),
+  carValidation(),
+  validate,
+  carsController.updateCar
+);
+router.delete("/:id", requiresAuth(), carsController.deleteCar);
 
 module.exports = router;
